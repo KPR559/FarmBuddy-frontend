3adb917046daa53fc304e200caa692e55300fce1
KPR559 <karetiprithvi@gmail.com>
2025-10-25 00:10:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,50 @@
 import React, { useState } from 'react';
 import LoginPage from './components/loginpage';
 import Signup from './components/signuppage';
-import LogoutButton from './components/logout';
+import Dashboard from './components/dashboard';
+import { getAuth } from 'firebase/auth';
+import ForgotPassword from './components/forgotpassword';
+import './css/App.css';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isSignedUp, setIsSignedUp] = useState(false);
-  const [LoginFail, setLoginFail] = useState(false);
+  const [showForgot, setShowForgot] = useState(false);
+  const user = getAuth().currentUser;
 
   return (
-    <div>
+    <div id ="app-container">
+      <video autoPlay loop muted id="background-video">
+        <source src="/videos/bg.mp4" type="video/mp4" />
+      </video>
       <h1>Welcome to FarmBuddy</h1>
       {!isLoggedIn ? (
         <>
+          <div id = "auth-container">
           {!isSignedUp ? (
             <>
-            <LoginPage onLogin={() => setIsLoggedIn(true)} onLoginFail={() => setLoginFail(true)} />
-            <p> Don't have an account {' '} <button onClick={() => setIsSignedUp(true)}>Sign Up</button> </p>
+            {!showForgot ? (
+              <>
+              <LoginPage onLogin={() => setIsLoggedIn(true)} />
+              <button onClick={() => setShowForgot(true)}> Forgot Password?</button>
+              <p> Don't have an account {' '} <button onClick={() => setIsSignedUp(true)}>Sign Up</button> </p>
+              </>
+            ):(
+              <>
+              {showForgot && <ForgotPassword onClose={() => setShowForgot(false)} />}
+              </>
+            )}
             </>
           ) : (
             <>
             <Signup />
-            <p> Already have an account? {' '} <button onClick={() => setIsSignedUp(false)}>Signin</button> </p>
+            <p> Already have an account? {' '} <button onClick={() => setIsSignedUp(false)}>Sign In</button> </p>
             </>)}
+          </div>
         </>
-      ) : (
+      ): (
         <div>
-          <LogoutButton onLogout={() => setIsLoggedIn(false)} />
+           <Dashboard user={user} onLogout={() => setIsLoggedIn(false)} />
         </div>
       )}
     </div>
